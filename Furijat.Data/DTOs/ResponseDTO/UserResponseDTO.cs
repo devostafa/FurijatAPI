5337148ddbd05f7fd56b82c02f3ab1b31c4d2bc6
devostafa <mostafa.maher98@gmail.com>
2026-03-28 17:16:13 +0200

    1- Removed .idea folder from being tracked 2- Renamed UserDTO to UserResponseDTO and refactored its properties 3- Moved PasswordHash service to services project (need to rethink how to assign seed passwords)

---
+++ Furijat.Data/DTOs/ResponseDTO/UserResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using Furijat.Data.DTOs.ResponseDTO;
+using Furijat.Data.Enums;
+using Furijat.Data.Models;
+
+namespace Furijat.Data.DTOs;
+
+public record UserResponseDTO(
+    Guid Id,
+    string Name,
+    UserTypeEnum UserType,
+    string PhoneNumber,
+    string Email,
+    string Facebook,
+    string X,
+    string Instagram,
+    ProjectResponseDTO Project
+    );
